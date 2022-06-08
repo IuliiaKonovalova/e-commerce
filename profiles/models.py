570958d73e5a3d60804c45b94337aac41f074e84
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-08 13:22:59 +0800

    Add Role model to profile app

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,31 @@
+"""Models for the profiles app."""
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Role(models.Model):
+    """Model for the roles."""
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        blank=False,
+        null=False,
+        verbose_name='Role name',
+        help_text=(
+            'format: required, max_length=50, unique=True, blank=False'
+        )
+    )
+    description = models.TextField(
+        max_length=500,
+        blank=True,
+        null=True,
+        verbose_name='Role description',
+        help_text=(
+            'format: not required, max_length=500'
+        )
+    )
+
+    def __str__(self):
+        """Return the name of the role."""
+        return self.name
