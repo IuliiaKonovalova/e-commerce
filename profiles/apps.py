0607931d232e967c4693cb27f88f333b7fa52423
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-08 15:20:05 +0800

    Add ready method to create profile on user

--- profiles/apps.py
+++ profiles/apps.py
@@ -1,6 +1,15 @@
+"""Apps for the profiles app."""
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 
 
 class ProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profiles'
+
+    def ready(self):
+        from profiles.signals import create_profile
+        from django.contrib.auth.models import User
+        post_save.connect(create_profile, sender=User)
+        from profiles.signals import save_profile
+        post_save.connect(save_profile, sender=User)
