e8512d855f7e436f4de896c03ddcbeb364ca39e9
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-08 15:18:37 +0800

    Add signals to create profile

---
+++ profiles/signals.py
@@ -0,0 +1,16 @@
+"""Signals for the profiles app."""
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from profiles.models import Profile
+from django.contrib.auth.models import User
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
