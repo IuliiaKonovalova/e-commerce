6fc67debed7705aaf786c5f36eeacd76879fb565
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-09 18:14:12 +0800

    Add UserProfileView

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+"""Views for the profiles app."""
+from django.views import View
+from django.shortcuts import render, get_object_or_404
+from .models import Role, Profile, Address
 
-# Create your views here.
+
+class UserProfileView(View):
+    """View for the profile page."""
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            my_profile = get_object_or_404(
+                Profile,
+                user=request.user
+            )
+            my_addresses = Address.objects.filter(
+                user=request.user
+            )
+            context = {
+                'my_profile': my_profile,
+                'my_addresses': my_addresses
+            }
+            return render(
+                request,
+                'profiles/my_profile.html',
+                context
+            )
+        else:
+            return render(
+                request,
+                'account/login.html'
+            )
