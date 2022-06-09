9bb50f3a3c6cea0364ad99f1408e6660f0c0d341
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-09 15:40:41 +0800

    Add AddressForm

--- profiles/forms.py
+++ profiles/forms.py
@@ -20,3 +20,27 @@ class ProfileForm(forms.ModelForm):
                 attrs={'class': 'form-check-input'}
             )
         }
+
+class AddressForm(forms.ModelForm):
+    """Form for the Address model."""
+    class Meta:
+        model = Address
+        fields = [
+            'country',
+            'county_region',
+            'city',
+            'address_line',
+            'zip_code',
+            'phone_number',
+            'is_primary'
+        ]
+        widgets = {
+            'country': forms.Select(attrs={'class': 'form-control'}),
+            'county_region': forms.Select(attrs={'class': 'form-control'}),
+            'city': forms.Select(attrs={'class': 'form-control'}),
+            'address_line': forms.TextInput(attrs={'class': 'form-control'}),
+            'zip_code': forms.NumberInput(attrs={'class': 'form-control'}),
+            'phone_number': forms.NumberInput(attrs={'class': 'form-control'}),
+            'is_primary': forms.CheckboxInput(attrs={'class': 'form-check-input'})
+        }
+
