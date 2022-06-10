8e99fae586fe5bbf974956125d5f729fa088f62f
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-10 14:37:59 +0800

    Add urls for editing profile views

--- profiles/tests/test_urls.py
+++ profiles/tests/test_urls.py
@@ -2,7 +2,10 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from profiles.views import (
-    UserProfileView
+    UserProfileView,
+    EditAvatarAjaxView,
+    ResetAvatarView,
+    EditUserProfileView
 )
 
 
@@ -13,3 +16,18 @@ class TestUrls(SimpleTestCase):
         """Test profile url."""
         url = reverse('my_profile', kwargs={'user': 'testuser'})
         self.assertEquals(resolve(url).func.view_class, UserProfileView)
+
+    def test_edit_avatar_url(self):
+        """Test edit avatar url."""
+        url = reverse('edit_avatar_ajax')
+        self.assertEquals(resolve(url).func.view_class, EditAvatarAjaxView)
+
+    def test_reset_avatar_url(self):
+        """Test reset avatar url."""
+        url = reverse('reset-avatar')
+        self.assertEquals(resolve(url).func.view_class, ResetAvatarView)
+
+    def test_edit_user_profile_url(self):
+        """Test edit user profile url."""
+        url = reverse('edit_profile', kwargs={'user': 'testuser'})
+        self.assertEquals(resolve(url).func.view_class, EditUserProfileView)
