bf659f671f88c64380db4c2066d4db0a41e529cb
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-10 14:38:32 +0800

    Add tested urls for editing profile vews

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,15 @@
 """Urls for the profile app."""
 from django.urls import path
-from .views import UserProfileView
+from .views import (
+    UserProfileView,
+    EditAvatarAjaxView,
+    ResetAvatarView,
+    EditUserProfileView
+)
 
 urlpatterns = [
     path('<str:user>/', UserProfileView.as_view(), name='my_profile'),
+    path('my_profile/edit_avatar/', EditAvatarAjaxView.as_view(), name='edit_avatar_ajax'),
+    path('my_profile/reset_avatar/', ResetAvatarView.as_view(), name='reset-avatar'),
+    path('<str:user>/edit/', EditUserProfileView.as_view(), name='edit_profile'),
 ]
