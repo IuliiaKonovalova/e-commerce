aba4d12bf9bfa4d013d941642ec5f52febb70f1f
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-10 17:17:01 +0800

    Add url for profile deletion functionality

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,12 +4,14 @@ from .views import (
     UserProfileView,
     EditAvatarAjaxView,
     ResetAvatarView,
-    EditUserProfileView
+    EditUserProfileView,
+    DeleteProfileView
 )
 
 urlpatterns = [
-    path('<str:user>/', UserProfileView.as_view(), name='my_profile'),
     path('my_profile/edit_avatar/', EditAvatarAjaxView.as_view(), name='edit_avatar_ajax'),
     path('my_profile/reset_avatar/', ResetAvatarView.as_view(), name='reset-avatar'),
+    path('my_profile/delete/', DeleteProfileView.as_view(), name='delete-user'),
+    path('<str:user>/', UserProfileView.as_view(), name='my_profile'),
     path('<str:user>/edit/', EditUserProfileView.as_view(), name='edit_profile'),
 ]
