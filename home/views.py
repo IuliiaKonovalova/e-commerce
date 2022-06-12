7813da9c7f9df3f77ada4277c74139b4c793c6e3
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-12 10:41:10 +0800

    Add view home page functionality

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
+from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
+
+class HomeView(View):
+    """View for the home page."""
+    def get(self, request):
+        """Return the home page."""
+        return render(request, 'home/home.html')
