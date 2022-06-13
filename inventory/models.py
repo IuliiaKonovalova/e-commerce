f48f411b72877661d7da45d18bd8b66cf2602057
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-13 14:17:49 +0800

    Add Brand model

--- inventory/models.py
+++ inventory/models.py
@@ -39,6 +39,7 @@ class Category(models.Model):
         ordering = ['name']
 
     def __str__(self):
+        """String representation of Category model"""
         return self.name
 
 
@@ -78,4 +79,52 @@ class Tag(models.Model):
         ordering = ['name']
 
     def __str__(self):
+        """String representation of Tag model"""
+        return self.name
+
+
+class Brand(models.Model):
+    """Brand model"""
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        unique=True,
+        blank=False,
+        verbose_name='Brand name',
+        help_text='format: required, max_length=100'
+    )
+    slug = models.SlugField(
+        max_length=150,
+        null=False,
+        unique=True,
+        blank=False,
+        verbose_name='Brand Slug',
+        help_text='format: required, max_length=150'
+    )
+    description = models.TextField(
+        max_length=500,
+        null=False,
+        blank=False,
+        verbose_name='Brand description',
+        help_text='format: required, max_length=500'
+    )
+    is_active = models.BooleanField(
+        default=False,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Created at'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Updated at'
+    )
+    class Meta:
+        """Meta class for Brand model"""
+        verbose_name = 'Brand'
+        verbose_name_plural = 'Brands'
+        ordering = ['name']
+
+    def __str__(self):
+        """String representation of the model"""
         return self.name
