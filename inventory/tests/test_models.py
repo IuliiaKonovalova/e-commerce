d5f84b5458515c945d7c99158e694b4b10b33aac
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-13 14:50:06 +0800

    Add testcases for product model

--- inventory/tests/test_models.py
+++ inventory/tests/test_models.py
@@ -3,7 +3,8 @@ from django.test import TestCase
 from inventory.models import (
     Category,
     Tag,
-    Brand
+    Brand,
+    Product,
 )
 import cloudinary
 import cloudinary.uploader
@@ -44,6 +45,24 @@ class TestModels(TestCase):
             slug='Adidas',
             is_active=False,
         )
+        self.product1 = Product.objects.create(
+            name='Nike Skirt',
+            slug='Nike-Skirt',
+            description='Nike Skirt',
+            category=self.category1,
+            brand=self.brand1,
+            is_active=True,
+        )
+        self.product1.tags.add(self.tag1)
+        self.product2 = Product.objects.create(
+            name='Adidas Shirt',
+            slug='Adidas-Shirt',
+            description='Adidas Shirt',
+            category=self.category2,
+            brand=self.brand2,
+            is_active=False,
+        )
+        self.product2.tags.add(self.tag2)
 
     def test_category_name(self):
         """Test the name field."""
@@ -90,3 +109,51 @@ class TestModels(TestCase):
         self.assertEqual(str(self.brand1), 'Nike')
         self.assertEqual(str(self.brand2), 'Adidas')
 
+    def test_product_name(self):
+        """Test the name field."""
+        self.assertEqual(self.product1.name, 'Nike Skirt')
+        self.assertEqual(self.product1.slug, 'Nike-Skirt')
+
+    def test_product_description(self):
+        """Test the description field."""
+        self.assertEqual(self.product1.description, 'Nike Skirt')
+        self.assertEqual(self.product2.description, 'Adidas Shirt')
+
+    def test_product_category(self):
+        """Test the category field."""
+        self.assertEqual(self.product1.category, self.category1)
+        self.assertEqual(self.product2.category, self.category2)
+
+    def test_product_tags(self):
+        """Test the tags field."""
+        self.assertQuerysetEqual(self.product1.tags.all(), [self.tag1])
+        self.assertQuerysetEqual(self.product2.tags.all(), [self.tag2])
+
+    def test_product_brand(self):
+        """Test the brand field."""
+        self.assertEqual(self.product1.brand, self.brand1)
+        self.assertEqual(self.product2.brand, self.brand2)
+
+    def test_product_is_active(self):
+        """Test product is active."""
+        self.assertEqual(self.product1.is_active, True)
+        self.assertEqual(self.product2.is_active, False)
+
+    def test_product_str(self):
+        """Test product string representation."""
+        self.assertEqual(str(self.product1), 'Nike Skirt')
+        self.assertEqual(str(self.product2), 'Adidas Shirt')
+
+    def test_get_active_products(self):
+        """Test get_active_products method."""
+        self.assertQuerysetEqual(
+            Product.get_active_products(),
+            [self.product1]
+        )
+
+    def test_get_not_active_products(self):
+        """Test get_not_active_products method."""
+        self.assertQuerysetEqual(
+            Product.get_not_active_products(),
+            [self.product2]
+        )
