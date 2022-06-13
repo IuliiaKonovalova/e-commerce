820c930fc61ffc4335d8ffa475f32c1a5f45b64a
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-13 14:01:38 +0800

    Add test cases for tag model

--- inventory/tests/test_models.py
+++ inventory/tests/test_models.py
@@ -1,6 +1,9 @@
 """Tests for the models in the inventory app."""
 from django.test import TestCase
-from inventory.models import Category
+from inventory.models import (
+    Category,
+    Tag,
+)
 import cloudinary
 import cloudinary.uploader
 
@@ -20,6 +23,16 @@ class TestModels(TestCase):
             slug='Food',
             is_active=True,
         )
+        self.tag1 = Tag.objects.create(
+            name='skirt',
+            slug='skirt',
+            is_active=True,
+        )
+        self.tag2 = Tag.objects.create(
+            name='shirt',
+            slug='shirt',
+            is_active=False,
+        )
 
     def test_category_name(self):
         """Test the name field."""
@@ -35,3 +48,23 @@ class TestModels(TestCase):
         """Test category string representation."""
         self.assertEqual(str(self.category1), 'Clothing')
         self.assertEqual(str(self.category2), 'Food')
+
+    def test_tag_name(self):
+        """Test the name field."""
+        self.assertEqual(self.tag1.name, 'skirt')
+        self.assertEqual(self.tag1.slug, 'skirt')
+
+    def test_tag_slug(self):
+        """Test tag slug."""
+        self.assertEqual(self.tag1.slug, 'skirt')
+        self.assertEqual(self.tag2.slug, 'shirt')
+
+    def test_tag_is_active(self):
+        """Test tag is active."""
+        self.assertEqual(self.tag1.is_active, True)
+        self.assertEqual(self.tag2.is_active, False)
+
+    def test_tag_str(self):
+        """Test tag string representation."""
+        self.assertEqual(str(self.tag1), 'skirt')
+        self.assertEqual(str(self.tag2), 'shirt')
