e4eac0a5c2a2b8e82b937d587f27d36ab93d7e79
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-13 13:44:56 +0800

    Add Category model

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,43 @@
+"""Models for the inventory app."""
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Category(models.Model):
+    """Category model"""
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        unique=False,
+        blank=False,
+        verbose_name='Category name',
+        help_text='format: required, max_length=100'
+    )
+    slug = models.SlugField(
+        max_length=150,
+        null=False,
+        unique=False,
+        blank=False,
+        verbose_name='Category Slug',
+        help_text='format: required, max_length=150'
+    )
+    is_active = models.BooleanField(
+        default=False,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Created at'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Updated at'
+    )
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
