c825ca3c725a4b96344ef80e9ebcdc9a4302855c
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-15 12:57:43 +0800

    Add ProductForm

--- inventory/forms.py
+++ inventory/forms.py
@@ -99,3 +99,66 @@ class BrandForm(forms.ModelForm):
                 attrs={'class': 'form-check-input'}
             )
         }
+
+
+class ProductForm(forms.ModelForm):
+    """Form for the Product model."""
+    class Meta:
+        model = Product
+        fields = [
+            'name',
+            'slug',
+            'description',
+            'category',
+            'tags',
+            'brand',
+            'is_active'
+        ]
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'title': 'max_length=50',
+                    'placeholder': 'Product Type Name',
+                }
+            ),
+            'slug': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'title': 'max_length=50',
+                    'placeholder': 'Product Type Slug Name',
+                }
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'title': 'max_length=500',
+                    'placeholder': 'Product Type Description',
+                }
+            ),
+            'category': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                    'title': 'select a category',
+                    'placeholder': 'Product Type Category',
+                }
+            ),
+            'tags': forms.SelectMultiple(
+                attrs={
+                    'class': 'form-control',
+                    'title': 'select tags',
+                    'placeholder': 'Product Type Tags',
+                }
+            ),
+            'brand': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                    'title': 'select a brand',
+                    'placeholder': 'Product Type Brand',
+                }
+            ),
+            'is_active': forms.CheckboxInput(
+                attrs={'class': 'form-check-input'}
+            )
+        }
+
