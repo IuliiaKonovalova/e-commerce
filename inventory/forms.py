33f45b4dc58292df5d8d01f68d23de144303bd27
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-15 12:38:44 +0800

    Add TagForm

--- inventory/forms.py
+++ inventory/forms.py
@@ -40,3 +40,29 @@ class CategoryForm(forms.ModelForm):
                 attrs={'class': 'form-check-input'}
             )
         }
+
+
+class TagForm(forms.ModelForm):
+    """Form for the Tag model."""
+    class Meta:
+        model = Tag
+        fields = ['name', 'slug', 'is_active']
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'title': 'max_length=50',
+                    'placeholder': 'Tag Name',
+                }
+            ),
+            'slug': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'title': 'max_length=50',
+                    'placeholder': 'Tag Slug Name',
+                }
+            ),
+            'is_active': forms.CheckboxInput(
+                attrs={'class': 'form-check-input'}
+            )
+        }
