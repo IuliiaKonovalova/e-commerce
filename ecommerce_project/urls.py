070816db829fefeeebfad8ad0688499903cded16
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-16 09:59:15 +0800

    Add url path for inventory app

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('profiles/', include('profiles.urls')),
+    path('inventory/', include('inventory.urls')),
 ]
