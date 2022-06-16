9a3a6c7989b1042084a2b8150b94370cc7eed6a1
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-16 16:30:29 +0800

    Add testcase to show the lowest store price

--- inventory/tests/test_models.py
+++ inventory/tests/test_models.py
@@ -454,6 +454,32 @@ class TestModels(TestCase):
         self.product_inventory3.save()
         self.assertEqual(self.product1.get_highest_price(), None)
 
+    def test_product_get_lowest_store_price(self):
+        """Test get_lowest_price method."""
+        self.assertEqual(self.product1.get_lowest_store_price(), 11.00)
+        self.product_inventory3 = ProductInventory.objects.create(
+            sku='11112',
+            upc='11112',
+            product=self.product1,
+            product_type=self.product_type1,
+            retail_price=10.00,
+            store_price=9.00,
+            sale_price=8.00,
+            weight=float(1.0),
+            is_active=True,
+        )
+        product_attr_value1 = ProductAttributeValue.objects.get(id=1)
+        product_attr_value2 = ProductAttributeValue.objects.get(id=2)
+        self.product_inventory3.attribute_values.set(
+            [product_attr_value1, product_attr_value2],
+        )
+        self.assertEqual(self.product1.get_lowest_store_price(), 9.00)
+        self.product_inventory1.is_active = False
+        self.product_inventory1.save()
+        self.product_inventory3.is_active = False
+        self.product_inventory3.save()
+        self.assertEqual(self.product1.get_lowest_store_price(), None)
+
     def test_product_image_name(self):
         """Test the name field."""
         self.assertEqual(self.product_image1.alt_text, 'Nike Skirt')
