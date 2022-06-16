47fbcfe212fe89dfe7539378f148e143a65d70c8
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-16 16:36:38 +0800

    Add method to show the highest store price

--- inventory/models.py
+++ inventory/models.py
@@ -354,6 +354,18 @@ class Product(models.Model):
         else:
             return None
 
+    def get_highest_store_price(self):
+        """Get highest price of product"""
+        active_sales_prices = ProductInventory.objects.filter(
+            product=self
+        ).filter(is_active=True)
+        if active_sales_prices.exists():
+            highest_price = active_sales_prices.order_by(
+                '-store_price'
+            ).first()
+            return highest_price.store_price
+        else:
+            return None
 
 
 class ProductImage(models.Model):
