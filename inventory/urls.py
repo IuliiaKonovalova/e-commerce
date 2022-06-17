bacccec56695a1483d8bd4971609b90de17aeb2a
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-17 12:40:28 +0800

    Add path for prudct detail page

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,10 +2,14 @@
 from django.urls import path
 from .views import (
     ProductsListView,
-
+    ProductDetailView,
 )
 
 urlpatterns = [
     path('store', ProductsListView.as_view(), name='products_list'),
-
+    path(
+        'store/<int:pk>',
+        ProductDetailView.as_view(),
+        name='product_detail'
+    ),
 ]
