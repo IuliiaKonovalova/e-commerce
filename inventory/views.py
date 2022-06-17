deb876503b1dff244770aae61ddf46358e545d4e
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-17 12:39:14 +0800

    Add ProductDetailView

--- inventory/views.py
+++ inventory/views.py
@@ -26,4 +26,17 @@ class ProductsListView(View):
         return render(request, 'inventory/products_list.html', context)
 
 
-
+class ProductDetailView(View):
+    """View for the product detail page."""
+    def get(self, request, *args, **kwargs):
+        """Handle GET requests."""
+        product = Product.objects.get(id=kwargs['pk'])
+        active_images = ProductImage.objects.filter(
+            product=product,
+            is_active=True
+        )
+        context = {
+            'product': product,
+            'active_images': active_images,
+        }
+        return render(request, 'inventory/product_detail.html', context)
