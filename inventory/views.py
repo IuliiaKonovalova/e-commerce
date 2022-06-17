42a3259d391bfe7d64c1963d8ec42b767fa78075
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-17 12:32:14 +0800

    Add ProductsListView

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,29 @@
+from django.views import View
 from django.shortcuts import render
+from .models import (
+    Category,
+    Tag,
+    Brand,
+    Product,
+    ProductImage,
+    ProductType,
+    ProductAttribute,
+    ProductAttributeValue,
+    ProductInventory,
+    Stock,
+)
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+class ProductsListView(View):
+    """View for the home page."""
+    def get(self, request, *args, **kwargs):
+        """Handle GET requests."""
+        products = Product.objects.all()
+        context = {
+            'products': products,
+        }
+        return render(request, 'inventory/products_list.html', context)
+
+
+
