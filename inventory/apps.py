95f7b068ba972e5102ab968286d60f5a9560bd03
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-19 20:04:15 +0800

    Add ready method

--- inventory/apps.py
+++ inventory/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 
 
 class InventoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'inventory'
+
+    def ready(self):
+        from inventory.signals import create_stock
+        from inventory.models import ProductInventory
+        post_save.connect(create_stock, sender=ProductInventory)
+        from inventory.signals import save_stock
+        post_save.connect(save_stock, sender=ProductInventory)
