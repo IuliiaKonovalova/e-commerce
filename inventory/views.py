c1a4470a48593e27396086557f6915d17afd440e
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-19 20:18:52 +0800

    Remove unused code from product Ajax view

--- inventory/views.py
+++ inventory/views.py
@@ -70,7 +70,6 @@ class ProductDetailView(View):
             product_inventory_active_stock = Stock.objects.filter(
                 product_inventory=product_inventory_active
             )
-            print(product_inventory_active_stock)
             if product_inventory_active_stock:
                 # Check if the product has stock
                 attr = product_inventory_active.product_type.\
@@ -125,54 +124,4 @@ class ProductAttributeAJAXView(View):
         if request.is_ajax():
             value = request.POST.get('value')
             product_id = request.POST.get('product_id')
-            # address = get_object_or_404(Address, id=address_id)
-            print(product_id)
             attribute = request.POST.get('attribute')
-            print(attribute)
-            products_inventory_active = ProductInventory.objects.filter(
-                product=product_id,
-                is_active=True,
-            )
-            print(products_inventory_active)
-            print('huy')
-            values_set_add = set()
-            attribute_values_set = set()
-            for product_inventory_active in products_inventory_active:
-                sth = product_inventory_active.attribute_values.all()
-                attr = product_inventory_active.product_type.product_type_attributes.all()
-                for attribute in attr:
-                    attribute_values_set.add(attribute)
-
-                print("all_values")
-                print(sth)
-                # print(sth.filter(attributevalues__in='red'))
-                selected_value = ProductAttributeValue.objects.get(
-                    attribute_value = value
-                )
-                print(selected_value.id)
-                print(selected_value.attribute_value)
-                if selected_value in sth:
-                    print('should be yellow')
-                    print(value)
-                    values_all = product_inventory_active.productattributevalues.all()
-                    print(values_all)
-                    for value1 in values_all:
-                        print('this is value1')
-                        print(value1)
-                        print('this is value1.attributevalues')
-                        print(value1.attributevalues)
-                        if value1.attributevalues == selected_value:
-                            print('should be yellow value')
-                            continue
-                        else:
-                            values_set_add.add(value1.attributevalues.attribute_value)
-                            print(values_set_add)
-                else:
-                    print('not yellow')
-            print(values_set_add)
-            # values_set_add to list
-            values_set_add_list = list(values_set_add)
-            print(values_set_add_list)
-            return JsonResponse({'success': True, 'values_set_add_list': values_set_add_list,})
-        else:
-            return JsonResponse({'success': False})
