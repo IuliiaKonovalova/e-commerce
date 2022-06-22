d59a2ae5b40dbbff51b686e0124c564c49a5ae82
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-22 18:01:13 +0800

    Add add_to_bag url path

--- bag/urls.py
+++ bag/urls.py
@@ -2,9 +2,15 @@
 from django.urls import path
 from .views import (
     BagDisplayView,
+    AddToBagAJAXView,
 )
 
 
 urlpatterns = [
     path('bag_display/', BagDisplayView.as_view(), name='bag_display'),
+    path(
+        'add_to_bag/',
+        AddToBagAJAXView.as_view(),
+        name='add_to_bag'
+    ),
 ]
