1b723466871db65c5c7d08a28279b77aae1eac77
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-22 18:00:50 +0800

    Add test case for add to bag url

--- bag/tests/test_urls.py
+++ bag/tests/test_urls.py
@@ -3,6 +3,7 @@ from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from bag.views import (
     BagDisplayView,
+    AddToBagAJAXView,
 )
 
 
@@ -13,3 +14,8 @@ class TestUrls(SimpleTestCase):
         """Test bag display url."""
         url = reverse('bag_display')
         self.assertEquals(resolve(url).func.view_class, BagDisplayView)
+
+    def test_add_to_bag_url(self):
+        """Test add to bag url."""
+        url = reverse('add_to_bag')
+        self.assertEquals(resolve(url).func.view_class, AddToBagAJAXView)
