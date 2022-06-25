7dd9c7ab1e053487d3a82406b94cf6a4bbd33727
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-25 13:12:53 +0800

    Add wishlist to admin pannel

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Wishlist
 
-# Register your models here.
+
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    """Admin for the Wishlist model."""
+    list_display = (
+        'user',
+        'created_at',
+    )
+    list_filter = (
+        'user',
+    )
+    search_fields = (
+        'user',
+        'product',
+        'created_at',
+        'updated_at'
+    )
