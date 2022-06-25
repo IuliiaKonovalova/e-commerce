4a93c7467f06e02a49f675958f8c6cd4487f247c
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-25 13:13:18 +0800

    Add signal for wishlist creation

--- profiles/signals.py
+++ profiles/signals.py
@@ -3,14 +3,17 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from profiles.models import Profile
 from django.contrib.auth.models import User
+from wishlist.models import Wishlist
 
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+        Wishlist.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+    instance.wishlist.save()
