2237f65b3a8c60a9969b62850c66efcf6e6917eb
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-28 07:43:24 +0800

    Add EmailNewsNotification model to admin

--- email_notifications/admin.py
+++ email_notifications/admin.py
@@ -1,3 +1,15 @@
+"""Admin for email_notifications."""
 from django.contrib import admin
+from .models import EmailNewsNotification
 
-# Register your models here.
+
+admin.site.register(EmailNewsNotification)
+class EmailNewsNotificationAdmin(admin.ModelAdmin):
+    """Admin for email news notifications."""
+    list_display = ('email_name', 'content', 'created_at')
+    list_filter = ('email_name',)
+    search_fields = ('email_name',)
+    ordering = ('-created_at',)
+    date_hierarchy = 'created_at'
+    list_per_page = 25
+    actions = ['send_email']
