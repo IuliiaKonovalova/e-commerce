5371c1a83f6b7eab766520271a73c56c55b0c60e
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-27 11:44:50 +0800

    Add testcases for promotion model

--- promotions/admin.py
+++ promotions/admin.py
@@ -1,3 +1,59 @@
+"""Admin for the Promotions model."""
 from django.contrib import admin
+from .models import Promotion
 
-# Register your models here.
+
+@admin.register(Promotion)
+class PromotionAdmin(admin.ModelAdmin):
+    """Admin for the Promotion model."""
+    list_display = (
+        'name',
+        'description',
+        'active',
+        'promotion_code',
+        'promotion_reduction',
+        'start_date',
+        'end_date',
+    )
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = (
+        'name',
+        'description',
+        'active',
+        'promotion_code',
+        'promotion_reduction',
+    )
+    search_fields = (
+        'name',
+        'description',
+        'active',
+        'promotion_code',
+        'promotion_reduction',
+    )
+    ordering = ('-created_at',)
+
+    def get_readonly_fields(self, request, obj=None):
+        """Set readonly fields."""
+        if obj:
+            return self.readonly_fields + ('created_at', 'updated_at')
+        return self.readonly_fields
+
+    def get_fieldsets(self, request, obj=None):
+        """Set fieldsets."""
+        if obj:
+            return self.fieldsets + (
+                ('Readonly fields', {'fields': ('created_at', 'updated_at')}),
+            )
+        return self.fieldsets
+
+    def get_list_display(self, request):
+        """Set list display."""
+        return self.list_display + ('created_at', 'updated_at')
+
+    def get_list_filter(self, request):
+        """Set list filter."""
+        return self.list_filter + ('created_at', 'updated_at')
+
+    def get_search_fields(self, request):
+        """Set search fields."""
+        return self.search_fields + ('created_at', 'updated_at')
--- promotions/models.py
+++ promotions/models.py
@@ -1,3 +1,85 @@
+"""Models for the promotions app."""
 from django.db import models
+# from django.utils import timezone
+from datetime import datetime
+from datetime import timedelta
+from decimal import Decimal
+from django.utils.text import slugify
+from django.core.validators import MinValueValidator, MaxValueValidator
+from inventory.models import ProductInventory
 
-# Create your models here.
+PERCENTAGE_VALIDATOR = [MinValueValidator(0), MaxValueValidator(100)]
+
+
+class Promotion(models.Model):
+    """Model for promotions."""
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        blank=False,
+        null=False,
+        verbose_name='Name',
+        help_text='Name of the promotion.'
+    )
+    slug = models.SlugField(
+        max_length=100,
+        unique=True,
+        blank=False,
+        null=False,
+        verbose_name='Slug',
+        help_text='Slug of the promotion.'
+    )
+    description = models.TextField(
+        max_length=700,
+        null=False,
+        blank=False,
+        verbose_name='Promotion description',
+        help_text='format: required, max_length=700'
+    )
+    promotion_code = models.CharField(
+        max_length=100,
+        unique=True,
+        blank=False,
+        null=False,
+        verbose_name='Promotion code',
+        help_text='Promotion code.'
+    )
+    promotion_reduction = models.DecimalField(
+        max_digits=3,
+        decimal_places=0,
+        default=Decimal(0),
+        validators=PERCENTAGE_VALIDATOR,
+    )
+    active = models.BooleanField(default=True)
+    start_date = models.DateTimeField(
+        verbose_name='Start date',
+    )
+    end_date = models.DateTimeField(
+        verbose_name='End date',
+    )
+    products_inventory_in_promotion = models.ManyToManyField(
+        ProductInventory,
+        blank=True,
+        related_name='products_promotions',
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Created at',
+        help_text='Date and time of creation.'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Updated at',
+        help_text='Date and time of last update.'
+    )
+
+    class Meta:
+        """Meta class for promotions."""
+        verbose_name = 'Promotion'
+        verbose_name_plural = 'Promotions'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        """Return the name of the promotion."""
+        return self.name
+
