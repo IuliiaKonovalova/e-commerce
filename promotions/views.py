29f2a83d273a674fecfcf75e0f4c37e13610854e
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-27 17:01:21 +0800

    Add PromotionsListView

--- promotions/views.py
+++ promotions/views.py
@@ -1,3 +1,32 @@
+"""Views for the promotions app."""
+from django.views import View
 from django.shortcuts import render
+from .models import Promotion
+from inventory.models import ProductInventory
 
-# Create your views here.
+
+class PromotionsListView(View):
+    """View for the promotions list page."""
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            print(request.user.profile.role, 'is authenticated')
+            if request.user.profile.role.id == 1:
+                return render(
+                    request,
+                    'profiles/access_denied.html',
+                )
+            else:
+                promotions = Promotion.objects.all()
+                context = {
+                    'promotions': promotions,
+                }
+                return render(
+                    request,
+                    'promotions/promotions_list.html',
+                    context
+                )
+        else:
+            return render(
+                request,
+                'account/login.html',
+            )
