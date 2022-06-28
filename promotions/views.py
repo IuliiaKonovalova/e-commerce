e94f4439451a5ca855bf64daac9c180e84f833b8
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-28 15:11:54 +0800

    Add AddPromotionView

--- promotions/views.py
+++ promotions/views.py
@@ -1,8 +1,9 @@
 """Views for the promotions app."""
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Promotion
 from inventory.models import ProductInventory
+from .forms import PromotionForm
 
 
 class PromotionsListView(View):
@@ -30,3 +31,59 @@ class PromotionsListView(View):
                 request,
                 'account/login.html',
             )
+
+
+class AddPromotionView(View):
+    """View for the add promotion page."""
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            print(request.user.profile.role, 'is authenticated')
+            if request.user.profile.role.id == 1:
+                return render(
+                    request,
+                    'profiles/access_denied.html',
+                )
+            else:
+                form = PromotionForm(request.POST)
+                return render(
+                    request,
+                    'promotions/add_promotion.html',
+                    {'form': form}
+                )
+        else:
+            return render(
+                request,
+                'account/login.html',
+            )
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            print(request.user.profile.role, 'is authenticated')
+            if request.user.profile.role.id == 1:
+                return render(
+                    request,
+                    'profiles/access_denied.html',
+                )
+            else:
+                form = PromotionForm(request.POST)
+                if form.is_valid():
+                    promotion = form.save(commit=False)
+                    print(form)
+                    print('products_inventory_in_promotion')
+                    products_inventory_in_promotion = form.cleaned_data[
+                        'products_inventory_in_promotion'
+                    ]
+                    print(products_inventory_in_promotion)
+                    promotion.save()
+                    promotion.products_inventory_in_promotion.set(
+                        products_inventory_in_promotion
+                    )
+                    promotion.save()
+                      
+                    return redirect('promotions_list')
+                else:
+                    return render(
+                        request,
+                        'promotions/add_promotion.html',
+                        {'form': form}
+                    )
