3b3b64d65d43e68bd5497873c6b7c873ff468dcc
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-06-29 19:55:03 +0800

    Change admin signals

--- email_notifications/admin.py
+++ email_notifications/admin.py
@@ -1,9 +1,10 @@
 """Admin for email_notifications."""
 from django.contrib import admin
-from .models import EmailNewsNotification
+from .models import EmailNewsNotification, StockEmailNotification
 
 
-admin.site.register(EmailNewsNotification)
+# admin.site.register(EmailNewsNotification)
+@admin.register(EmailNewsNotification)
 class EmailNewsNotificationAdmin(admin.ModelAdmin):
     """Admin for email news notifications."""
     list_display = ('email_name', 'content', 'created_at')
@@ -13,3 +14,21 @@ class EmailNewsNotificationAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_at'
     list_per_page = 25
     actions = ['send_email']
+
+# admin.site.register(StockEmailNotification)
+@admin.register(StockEmailNotification)
+class StockEmailNotification(admin.ModelAdmin):
+    """Admin for product detail view."""
+    list_display = (
+        'user',
+        'requested_product',
+        'requested_quantity',
+        'created_at',
+        'answer_sent',
+    )
+    list_filter = ('user', 'requested_product')
+    search_fields = ('user', 'requested_product')
+    ordering = ('-created_at',)
+    date_hierarchy = 'created_at'
+    list_per_page = 25
+
