b6be50f193f5df1e08809ccaafa94e83bc2c1c19
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-09 15:12:33 +0800

    Delete slug field from Tag form

--- personnel/forms.py
+++ personnel/forms.py
@@ -39,20 +39,13 @@ class TagForm(forms.ModelForm):
     """Form for the Tag model."""
     class Meta:
         model = Tag
-        fields = ['name', 'slug', 'is_active']
+        fields = ['name', 'is_active']
         widgets = {
             'name': forms.TextInput(
                 attrs={
                     'class': 'form-control',
                     'title': 'max_length=50',
                     'placeholder': 'Tag Name',
-                }
-            ),
-            'slug': forms.TextInput(
-                attrs={
-                    'class': 'form-control',
-                    'title': 'max_length=50',
-                    'placeholder': 'Tag Slug Name',
                 }
             ),
             'is_active': forms.CheckboxInput(
