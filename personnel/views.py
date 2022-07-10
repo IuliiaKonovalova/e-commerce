e1ac0ba7b2a3b0ec8e1b0da07af79a38d2cd8467
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-10 16:16:03 +0800

    Add adding stock to a unit functionality

--- personnel/views.py
+++ personnel/views.py
@@ -26,6 +26,7 @@ from .forms import (
     CategoryForm,
     BrandForm,
     TagForm,
+    StockForm,
 )
 
 
@@ -1583,6 +1584,102 @@ class StockView(View):
                     request,
                     'personnel/stock.html',
                     context,
+                )
+        else:
+            return render(
+                request,
+                'account/login.html',
+            )
+
+
+class AddStockView(View):
+    """View for the add stock page."""
+    def get(self, request, *args, **kwargs):
+        """Handle GET requests."""
+        if request.user.is_authenticated:
+            # Check if user is a customer
+            if request.user.profile.role.id == 3:
+                # get product inventory id from url
+                inventory_pk = kwargs['inventory_pk']
+                pk = kwargs['pk']
+                # get product inventory
+                product_inventory = get_object_or_404(
+                    ProductInventory,
+                    id=inventory_pk
+                )
+                # get product
+                product = get_object_or_404(Product, id=pk)
+                form = StockForm()
+                context = {
+                    'form': form,
+                    'inventory_pk': inventory_pk,
+                    'pk': pk,
+                    'product_inventory': product_inventory,
+                    'product': product,
+                }
+                return render(
+                    request,
+                    'personnel/add_stock.html',
+                    context,
+                )
+            else:
+                return render(
+                    request,
+                    'profiles/access_denied.html',
+                )
+        else:
+            return render(
+                request,
+                'account/login.html',
+            )
+    def post(self, request, *args, **kwargs):
+        """Handle POST requests."""
+        if request.user.is_authenticated:
+            # Check if user is a customer
+            if request.user.profile.role.id == 3:
+                inventory_pk = kwargs['inventory_pk']
+                pk = kwargs['pk']
+                # prefill form with product inventory
+                inventory = get_object_or_404(
+                    ProductInventory,
+                    id=inventory_pk
+                )
+                # get product inventory
+                product_inventory = get_object_or_404(
+                    ProductInventory,
+                    id=inventory_pk
+                )
+                # get product
+                product = get_object_or_404(Product, id=pk)
+                form = StockForm(request.POST)
+                context = {
+                    'form': form,
+                    'inventory_pk': inventory_pk,
+                    'pk': pk,
+                    'product_inventory': product_inventory,
+                    'product': product,
+                }
+                if form.is_valid():
+                    stock = form.save(commit=False)
+                    stock.product_inventory = inventory
+                    form.save()
+                    # redirect to product/<int:pk>/inventory/<int:inventory_pk>/
+                    return HttpResponseRedirect(
+                        '/personnel/product/{}/inventory/{}/'.format(
+                            pk,
+                            inventory_pk,
+                        )
+                    )
+                else:
+                    return render(
+                        request,
+                        'personnel/add_stock.html',
+                        context,
+                    )
+            else:
+                return render(
+                    request,
+                    'profiles/access_denied.html',
                 )
         else:
             return render(
