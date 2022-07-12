ae7f7f018166eed69f36392e395b44c28668e781
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-12 20:08:36 +0800

    Add my_order_details url path

--- orders/urls.py
+++ orders/urls.py
@@ -2,10 +2,27 @@
 from django.urls import path
 from .views import (
     OrdersView,
+    AddOrderAJAXView,
+    UserOrdersView,
+    UserOrderDetailsView,
 )
 
 
 urlpatterns = [
     path('', OrdersView.as_view(), name='orders'),
-
+    path(
+        'add/',
+        AddOrderAJAXView.as_view(),
+        name='add'
+    ),
+    path(
+        '<str:user>/my_orders/',
+        UserOrdersView.as_view(),
+        name='my_orders'
+    ),
+    path(
+        '<str:user>/my_orders/<int:order_id>/',
+        UserOrderDetailsView.as_view(),
+        name='my_order_details'
+    ),
 ]
