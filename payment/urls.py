b29d1becddad48fe120b514a64b6e38a9503415d
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-12 13:43:33 +0800

    Add order_placed url path

--- payment/urls.py
+++ payment/urls.py
@@ -2,11 +2,13 @@
 from django.urls import path
 from .views import (
     BasketView,
+    order_placed,
 
 )
 
 
 urlpatterns = [
     path('', BasketView, name='payment'),
+    path('order_placed/', order_placed, name='order_placed'),
 
 ]
