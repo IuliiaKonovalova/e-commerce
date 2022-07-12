52b685cca46586351a9ed1620d195baf368d5546
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-12 15:46:19 +0800

    Add ajax view for order creation functionality

--- orders/views.py
+++ orders/views.py
@@ -29,3 +29,57 @@ class OrdersView(View):
                 request,
                 'account/login.html',
             )
+
+
+class AddOrderAJAXView(View):
+    """View for adding order AJAX."""
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            if request.is_ajax():
+                print(request.POST)
+                user = request.user
+                full_name = request.POST.get('full_name')
+                email = request.POST.get('email')
+                phone = request.POST.get('phone')
+                address1 = request.POST.get('address1')
+                address2 = request.POST.get('address2')
+                country = request.POST.get('country')
+                county_region_state = request.POST.get('county_region_state')
+                city = request.POST.get('city')
+                zip_code = request.POST.get('zip_code')
+                order_key = request.POST.get('order_key')
+                bag = bag_contents(request)
+                total_paid = str(bag['total'])
+                bag_items = bag['bag_items']
+                if Order.objects.filter(order_key=order_key).exists():
+                    pass
+                else:
+                    order = Order.objects.create(
+                        user=user,
+                        full_name=full_name,
+                        email=email,
+                        phone=phone,
+                        address1=address1,
+                        address2=address2,
+                        country=country,
+                        county_region_state=county_region_state,
+                        city=city,
+                        zip_code=zip_code,
+                        order_key=order_key,
+                        total_paid=total_paid,
+                    )
+                    print(order)
+                    for item in bag_items:
+                        item = OrderItem.objects.create(
+                            order=order,
+                            product_inventory=item['product_inventory'],
+                            quantity=item['quantity'],
+                        )
+                        print(item)
+                return JsonResponse({'success': True})
+            return JsonResponse({'success': False})
+        else:
+            return render(
+                request,
+                'account/login.html',
+            )
