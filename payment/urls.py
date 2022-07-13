8b759322bb986768ec0521842585bf509d02bd9c
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-13 16:39:26 +0800

    Add webhook url path

--- payment/urls.py
+++ payment/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from .views import (
     BasketView,
     order_placed,
-
+    stripe_webhook,
 )
 
 
 urlpatterns = [
     path('', BasketView, name='payment'),
     path('order_placed/', order_placed, name='order_placed'),
-
+    path('webhook/', stripe_webhook),
 ]
