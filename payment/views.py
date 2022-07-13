62c691416a511abe3dc5a8fb16e02b788576d295
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-13 16:44:52 +0800

    Add stripe webhook handling functionality

--- payment/views.py
+++ payment/views.py
@@ -71,3 +71,19 @@ def order_placed(request):
     return render(request, 'payment/order_placed.html')
 
 
+@csrf_exempt
+def stripe_webhook(request):
+    payload = request.body
+    event = None
+    try:
+        event = stripe.Event.construct_from(
+            json.loads(payload), stripe.api_key
+        )
+    except ValueError as e:
+        return HttpResponse(status=400)
+    # Handle the event
+    if event.type == 'payment_intent.succeeded':
+        payment_confirmation(event.data.object.client_secret)
+    else:
+        print('Unhandled event type {}'.format(event.type))
+    return HttpResponse(status=200)
