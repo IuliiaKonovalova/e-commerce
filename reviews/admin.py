a2379fe58c105cf3a74e9f17c368b09c2b494247
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-17 20:02:31 +0800

    Add admin models for admin dashboard

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,24 @@
+"""Admin for reviews app."""
 from django.contrib import admin
+from .models import Review, ReviewImage
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    """Admin for the Review model."""
+    list_display = (
+        'user',
+        'product',
+        'rating',
+        'comment',
+    )
+    list_filter = ('user', 'product', 'rating', 'comment')
+    search_fields = ('user', 'product', 'rating', 'comment')
+
+
+@admin.register(ReviewImage)
+class ReviewImageAdmin(admin.ModelAdmin):
+    """Admin for the ReviewImage model."""
+    list_display = ('review', 'image')
+    list_filter = ('review', 'image')
+    search_fields = ('review', 'image')
