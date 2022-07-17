386b01cabdc5350baff3f83cb3f7e118a9292c6a
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-17 18:12:53 +0800

    Add test for review details url path

--- reviews/tests/test_urls.py
+++ reviews/tests/test_urls.py
@@ -2,12 +2,22 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from reviews.views import (
+    ReviewDetailView,
     AddReviewView,
+
 )
 
 
 class TestUrls(SimpleTestCase):
     """Test reviews urls"""
+    def test_review_url(self):
+        """Test review url"""
+        url = reverse(
+            'review',
+            kwargs={'order_id': 1, 'product_id': 1}
+        )
+        self.assertEquals(resolve(url).func.view_class, ReviewDetailView)
+
     def test_add_review_url(self):
         """Test add review url"""
         url = reverse(
@@ -15,3 +25,5 @@ class TestUrls(SimpleTestCase):
             kwargs={'order_id': 1, 'product': 1}
         )
         self.assertEquals(resolve(url).func.view_class, AddReviewView)
+
+
