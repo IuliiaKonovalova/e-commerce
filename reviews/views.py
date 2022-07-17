2a35312e06a51f195a5072debc78d8a6e6897e0d
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-17 18:20:28 +0800

    Add view review functionality

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+"""Views for reviews app."""
+from django.views import View
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.core.paginator import Paginator
+from orders.models import Order
+from reviews.models import Review, ReviewImage
+from inventory.models import Product
 
-# Create your views here.
+
+class ReviewDetailView(View):
+    """View for review detail."""
+    def get(self, request, *args, **kwargs):
+        order = get_object_or_404(Order, id=kwargs['order_id'])
+        product = get_object_or_404(Product, id=self.kwargs['product_id'])
+        # get review
+        review = get_object_or_404(
+            Review,
+            order=order,
+            product=product,
+        )
+        images = ReviewImage.objects.filter(review=review)
+        context = {
+            'product': product,
+            'review': review,
+            'images': images,
+        }
+        return render(request, 'reviews/review_detail.html', context)
+
