bdc5332deaa9acf1d07648a70a0cc632ff001afe
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-17 20:00:11 +0800

    Add url for AddReviewWithImagesAJAXView

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,11 +1,26 @@
 """URLs for the reviews app."""
 from django.urls import path
 from .views import (
+    ReviewDetailView,
     AddReviewView,
+    AddReviewWithImagesAJAXView,
 )
 
 
 urlpatterns = [
-    path('add_review/<int:order_id>/<int:product>/',
-    AddReviewView.as_view(), name='add_review'),
+    path(
+        'review/<int:order_id>/<int:product_id>/',
+        ReviewDetailView.as_view(),
+        name='review',
+    ),
+    path(
+        'add_review/<int:order_id>/<int:product_id>/',
+        AddReviewView.as_view(),
+        name='add_review'
+    ),
+    path(
+        'add_review_with_images_ajax/',
+        AddReviewWithImagesAJAXView.as_view(),
+        name='add_review_with_images_ajax'
+    ),
 ]
