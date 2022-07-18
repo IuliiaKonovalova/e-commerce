e72c8ba0bd024557fabcae7b819a4d1c85b97816
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-18 17:46:37 +0800

    Add test for add_promo_email url

--- email_notifications/tests/test_urls.py
+++ email_notifications/tests/test_urls.py
@@ -3,12 +3,23 @@ from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from email_notifications.views import (
     EmailStockNotificationFormAJAX,
+    PromoEmailCreateView,
 )
 
 
 class TestUrls(SimpleTestCase):
+    """Test for email_notifications/urls.py"""
+
     def test_add_to_stock_email_notification_url(self):
+        """Test for email_notifications/urls.py"""
         url = reverse('add_to_stock_email_notification')
         self.assertEquals(
             resolve(url).func.view_class, EmailStockNotificationFormAJAX
+        )
+
+    def test_add_promo_email_url(self):
+        """Test for email_notifications/urls.py"""
+        url = reverse('add_promo_email')
+        self.assertEquals(
+            resolve(url).func.view_class, PromoEmailCreateView
         )
