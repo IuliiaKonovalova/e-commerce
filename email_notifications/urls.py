c668760ff21b7ce9a48051688c0ff6284363da20
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-18 17:47:15 +0800

    Add url path to manage promo email creation

--- email_notifications/urls.py
+++ email_notifications/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from .views import (
     EmailStockNotificationFormAJAX,
+    PromoEmailCreateView,
 )
 
 
@@ -10,5 +11,10 @@ urlpatterns = [
         'add_to_stock_email_notification/',
         EmailStockNotificationFormAJAX.as_view(),
         name='add_to_stock_email_notification',
+    ),
+    path(
+        'add_promo_email/',
+        PromoEmailCreateView.as_view(),
+        name='add_promo_email',
     ),
 ]
