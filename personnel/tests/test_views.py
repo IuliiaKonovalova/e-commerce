9a023da3172d767e747dcde4fffb4cbd72ff2137
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-21 15:31:15 +0800

    Add test cases for EditAttributeValueView

--- personnel/tests/test_views.py
+++ personnel/tests/test_views.py
@@ -324,6 +324,10 @@ class TestViews(TestCase):
         self.add_attribute_value_url = reverse(
             'add_attribute_value'
         )
+        self.edit_attribute_value_url = reverse(
+            'edit_attribute_value',
+            kwargs={'pk': 1}
+        )
 
     def test_products_table_view_user_logged_out(self):
         """Test products table view user logged out."""
@@ -3558,6 +3562,102 @@ class TestViews(TestCase):
         self.assertTrue(ProductAttributeValue.objects.count() == 2)
         response = self.client.post(
             self.add_attribute_value_url,
+            {
+                'product_attribute': '',
+                'attribute_value': 'banana',
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(ProductAttributeValue.objects.count() == 2)
+
+    def test_edit_attribute_value_get_view_user_logged_out(self):
+        """Test edit attribute value get view user logged out"""
+        response = self.client.get(
+            self.edit_attribute_value_url,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/login.html')
+
+    def test_edit_attribute_value_get_view_user_without_access(self):
+        """Test edit attribute value get view user logged in without access"""
+        self.client.force_login(self.user)
+        response = self.client.get(
+            self.edit_attribute_value_url,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profiles/access_denied.html')
+        self.client.logout()
+
+    def test_edit_attribute_value_get_view_user_with_access(self):
+        """Test edit attribute value get view user logged in with access"""
+        self.client.force_login(self.user3)
+        self.assertFalse(self.profile3.role.id == 2)
+        self.profile3 = Profile.objects.get(id=self.user3.profile.id)
+        self.profile3.role = self.role3
+        self.profile3.save()
+        response = self.client.get(self.edit_attribute_value_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response,
+            'personnel/edit_attribute_value.html'
+        )
+
+    def test_edit_attribute_value_post_view_user_logged_out(self):
+        """Test edit attribute value post view user logged out"""
+        response = self.client.post(
+            self.edit_attribute_value_url,
+            {
+                'product_attribute': self.product_attribute1.id,
+                'attribute_value': 'banana',
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/login.html')
+
+    def test_edit_attribute_value_post_view_user_without_access(self):
+        """Test edit attribute value post view user logged in without access"""
+        self.client.force_login(self.user)
+        response = self.client.post(
+            self.edit_attribute_value_url,
+            {
+                'product_attribute': self.product_attribute1.id,
+                'attribute_value': 'banana',
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profiles/access_denied.html')
+        self.client.logout()
+
+    def test_edit_attribute_value_post_view_user_with_access(self):
+        """Test edit attribute value post view user logged in with access"""
+        self.client.force_login(self.user3)
+        self.assertFalse(self.profile3.role.id == 2)
+        self.profile3 = Profile.objects.get(id=self.user3.profile.id)
+        self.profile3.role = self.role3
+        self.profile3.save()
+        # count the number of attribute values before adding
+        self.assertTrue(ProductAttributeValue.objects.count() == 2)
+        response = self.client.post(
+            self.edit_attribute_value_url,
+            {
+                'product_attribute': self.product_attribute1.id,
+                'attribute_value': 'banana',
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(ProductAttributeValue.objects.count() == 2)
+
+    def test_edit_attribute_value_post_view_user_with_access_failed(self):
+        """Test edit attribute value post view user logged in with access"""
+        self.client.force_login(self.user3)
+        self.assertFalse(self.profile3.role.id == 2)
+        self.profile3 = Profile.objects.get(id=self.user3.profile.id)
+        self.profile3.role = self.role3
+        self.profile3.save()
+        # count the number of attribute values before adding
+        self.assertTrue(ProductAttributeValue.objects.count() == 2)
+        response = self.client.post(
+            self.edit_attribute_value_url,
             {
                 'product_attribute': '',
                 'attribute_value': 'banana',
