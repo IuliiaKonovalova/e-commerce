fe19480d867525fbd01880b6fb28eddf7282c8f1
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-22 08:01:53 +0800

    Add tests for DeleteValue view

--- personnel/tests/test_views.py
+++ personnel/tests/test_views.py
@@ -328,6 +328,10 @@ class TestViews(TestCase):
             'edit_attribute_value',
             kwargs={'pk': 1}
         )
+        self.delete_attribute_value_url = reverse(
+            'delete_attribute_value',
+            kwargs={'pk': 1}
+        )
 
     def test_products_table_view_user_logged_out(self):
         """Test products table view user logged out."""
@@ -3665,3 +3669,35 @@ class TestViews(TestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertTrue(ProductAttributeValue.objects.count() == 2)
+
+    def test_delete_attribute_value_get_view_user_logged_out(self):
+        """Test delete attribute value get view user logged out"""
+        response = self.client.get(
+            self.delete_attribute_value_url,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/login.html')
+
+    def test_delete_attribute_value_get_view_user_without_access(self):
+        """Test delete value get view user logged in without access"""
+        self.client.force_login(self.user)
+        response = self.client.get(
+            self.delete_attribute_value_url,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profiles/access_denied.html')
+        self.client.logout()
+
+    def test_delete_attribute_value_get_view_user_with_access(self):
+        """Test delete attribute value get view user logged in with access"""
+        self.client.force_login(self.user3)
+        self.assertFalse(self.profile3.role.id == 2)
+        self.profile3 = Profile.objects.get(id=self.user3.profile.id)
+        self.profile3.role = self.role3
+        self.profile3.save()
+        response = self.client.get(self.delete_attribute_value_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response,
+            'personnel/delete_attribute_value.html'
+        )
