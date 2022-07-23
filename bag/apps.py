a13f4ab53f0c276845b80ff100d399dd720e165b
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-23 13:40:31 +0800

    Add docstrings to bag apps.py

--- bag/apps.py
+++ bag/apps.py
@@ -1,6 +1,8 @@
+"""App for bag app"""
 from django.apps import AppConfig
 
 
 class BagConfig(AppConfig):
+    """Config for bag app"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bag'
