c11f065aed5cf431d5424397e535a79b1e405b5b
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-23 13:09:41 +0800

    Add doc strings to apps.py wishlist app

--- wishlist/apps.py
+++ wishlist/apps.py
@@ -1,6 +1,8 @@
+"""App for Wishlist."""
 from django.apps import AppConfig
 
 
 class WishlistConfig(AppConfig):
+    """Config for Wishlist."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'wishlist'
