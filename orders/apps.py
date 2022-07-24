a481b66d7a61e3cb063df3823a05ba389f5fde82
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-24 16:02:01 +0800

    Add doc strings to order apps.py

--- orders/apps.py
+++ orders/apps.py
@@ -1,6 +1,8 @@
+"""Apps.py for orders app."""
 from django.apps import AppConfig
 
 
 class OrdersConfig(AppConfig):
+    """Config for orders app."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'orders'
