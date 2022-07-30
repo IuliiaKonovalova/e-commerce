01e20ee1eb8e7f2662d6d94e04aa8ad37ffb90af
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-30 16:46:59 +0800

    Add docstrings to promotions apps.py

--- promotions/apps.py
+++ promotions/apps.py
@@ -1,6 +1,8 @@
+"""Apps.py for promotions app."""
 from django.apps import AppConfig
 
 
 class PromotionsConfig(AppConfig):
+    """Config for promotions app."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'promotions'
