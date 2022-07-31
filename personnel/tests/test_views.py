6d9470deefae1c421bf55ffa69cc66ff8f1d1df0
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-07-31 18:18:53 +0800

    Add test cases for seearch brand functionality

--- personnel/tests/test_views.py
+++ personnel/tests/test_views.py
@@ -1836,6 +1836,38 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, 'personnel/brands_table.html')
         self.client.logout()
 
+    def test_brands_table_get_view_staff_search_query(self):
+        """Test brands table get view user with access"""
+        self.client.force_login(self.user2)
+        self.assertFalse(self.profile2.role.id == 1)
+        self.profile2 = Profile.objects.get(id=self.user2.profile.id)
+        self.profile2.role = self.role2
+        self.profile2.save()
+        response = self.client.get(
+            self.brands_table_url,
+            {'search_query': 'Nike'},
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'personnel/brands_table.html')
+        self.assertEqual(len(response.context['brands']), 1)
+        self.client.logout()
+
+    def test_brands_table_get_view_staff_search_query_no_results(self):
+        """Test brands table get view user with access"""
+        self.client.force_login(self.user2)
+        self.assertFalse(self.profile2.role.id == 1)
+        self.profile2 = Profile.objects.get(id=self.user2.profile.id)
+        self.profile2.role = self.role2
+        self.profile2.save()
+        response = self.client.get(
+            self.brands_table_url,
+            {'search_query': ''},
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'personnel/brands_table.html')
+        self.assertEqual(len(response.context['brands']), 2)
+        self.client.logout()
+
     def test_brand_detail_view_user_logged_out(self):
         """Test brand detail view user logged out"""
         response = self.client.get(
