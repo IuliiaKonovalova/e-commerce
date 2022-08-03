9091743069373eefd32a46b1365389860fce14e6
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-08-03 13:32:19 +0800

    Add functions handlers for 400 and 505 errors

---
+++ ecommerce_project/views.py
@@ -0,0 +1,13 @@
+
+"""Views to handle errors"""
+from django.shortcuts import render
+
+
+def handler404(request, exception):
+    """ Error Handler 404 - Page Not Found """
+    return render(request, "errors/404.html", status=404)
+
+
+def handler500(request):
+    """ Error Handler 500 - Internal Server Error """
+    return render(request, "errors/500.html", status=500)
