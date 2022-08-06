c8c75052551ec4df515694f610ba289e88a4712b
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-08-06 15:04:27 +0800

    Add docstrings to emails apps.py

--- email_notifications/apps.py
+++ email_notifications/apps.py
@@ -1,6 +1,8 @@
+"""Apps for the email_notifications app."""
 from django.apps import AppConfig
 
 
 class EmailNotificationsConfig(AppConfig):
+    """Config for email_notifications app."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'email_notifications'
