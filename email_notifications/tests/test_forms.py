35dbe10d6bbe5dc6d93bf1e553c44910f34d4242
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-08-06 14:59:55 +0800

    Add missing blank line after test function

--- email_notifications/tests/test_forms.py
+++ email_notifications/tests/test_forms.py
@@ -9,6 +9,7 @@ class EmailNewsNotificationFormTest(TestCase):
         """Test for email news notification form has fields"""
         form = EmailNewsNotificationForm()
         self.assertEqual(len(form.fields), 2)
+
     def test_email_news_notification_form_is_valid(self):
         """Test for email news notification form is valid"""
         form = EmailNewsNotificationForm({
