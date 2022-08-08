c83a0e82c28364eb4662c18592cba9a58a13c410
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-08-09 07:45:48 +0800

    Add docstrings to personnel apps and admin files

--- personnel/admin.py
+++ personnel/admin.py
@@ -1,3 +1,2 @@
+"""Admin for the personnel app"""
 from django.contrib import admin
-
-# Register your models here.
--- personnel/apps.py
+++ personnel/apps.py
@@ -1,6 +1,8 @@
+"""Apps for personnel app"""
 from django.apps import AppConfig
 
 
 class PersonnelConfig(AppConfig):
+    """AppConfig for personnel app"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'personnel'
