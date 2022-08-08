19ea119a21c9fe2b945786de7719404a63b11279
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-08-08 10:35:02 +0800

    Add docstrings to inventory apps.py file

--- inventory/apps.py
+++ inventory/apps.py
@@ -1,7 +1,9 @@
+"""Apps for inventory app"""
 from django.apps import AppConfig
 from django.db.models.signals import post_save
 
 
 class InventoryConfig(AppConfig):
+    """AppConfig for inventory app"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'inventory'
