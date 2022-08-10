81df52d34dcd59ced49c13ef6bc8e0e7cc1e6573
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-08-10 08:52:23 +0800

    Add timeout for django messages

--- static/js/base.js
+++ static/js/base.js
@@ -3,7 +3,13 @@ $(document).ready(function () {
   $('.carousel').carousel();
   // set bootstrap carousel to stop autoplaying
   $('.carousel').carousel({
-
     ride: true
   });
+  // Function controls messages's display
+  setTimeout(() => {
+    let messages = $('#messages-notes');
+    if (messages) {
+      $('#messages-notes').remove();
+    }
+  }, 2500);
 });
