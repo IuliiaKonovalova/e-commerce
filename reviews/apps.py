54599be0398e97364c3cb9f0c47a4bfef27b2c8e
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-08-11 15:10:56 +0800

    Add docstrings reviews apps.py

--- reviews/apps.py
+++ reviews/apps.py
@@ -1,6 +1,8 @@
+"""Apps.py for the reviews app."""
 from django.apps import AppConfig
 
 
 class ReviewsConfig(AppConfig):
+    """Config for the reviews app."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reviews'
