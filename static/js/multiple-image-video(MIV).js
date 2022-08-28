966e35545d303ec2bec882b8edaf87faafefc6da
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-08-28 08:12:26 +0800

    Fix semicolons in multiple image js code

--- static/js/multiple-image-video(MIV).js
+++ static/js/multiple-image-video(MIV).js
@@ -13,8 +13,8 @@
                 var file = $(this).get(0);
                 var preview = window.URL.createObjectURL(file.files[0]);
                 $('.gallery').append("<div class='apnd-img'><img src='" + preview + "' id='img" + i + "' class='img-responsive'><i class='fa fa-close delfile'></i></div>");
-                i++
-            })
+                i++;
+            });
         });
         $(document).on('click', elemRef.video, function () {
             $(elemRef.video).after("<input type='file' style='display:none;' id='vidupload" + j + "' name='video[" + j + "]'/>");
@@ -23,18 +23,18 @@
                 var file = $(this).get(0);
                 var preview = window.URL.createObjectURL(file.files[0]);
                 $('.gallery').append("<div class='apnd-img'><iframe width='50' height='50' src='" + preview + "' id='vid" + j + "' frameborder='0' allowfullscreen></iframe><i class='fa fa-close delfile1'></i></div>");
-                j++
-            })
+                j++;
+            });
         });
         $(document).on('click', '.delfile', function () {
             var elem = $(this).prev().attr('id').substr(3, 4);
             $(this).parent().remove();
-            $('#imgupload' + elem).remove()
+            $('#imgupload' + elem).remove();
         });
         $(document).on('click', '.delfile1', function () {
             var elem = $(this).prev().attr('id').substr(3, 4);
             $(this).parent().remove();
-            $('#vidupload' + elem).remove()
-        })
-    }
-}(jQuery))
+            $('#vidupload' + elem).remove();
+        });
+    };
+}(jQuery));
